8c1c18a6403a247c441e9c6d1f04b68cc0696937
gulnazkyzy <gulnazkyzy@yahoo.com>
2024-10-08 19:34:08 +0000

    added output.tf

---
+++ Class2/output.tf
@@ -0,0 +1,23 @@
+output "ami" {
+  value = aws_instance.web.ami
+}
+
+output "arn" {
+  value = aws_instance.web.arn
+}
+
+output "id" {
+  value = aws_instance.web.id
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "instance_state" {
+  value = aws_instance.web.instance_state
+}
+
+output "public_dns" {
+  value = aws_instance.web.public_dns
+}
